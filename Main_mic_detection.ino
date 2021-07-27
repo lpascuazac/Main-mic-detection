b6ed9f63392d45abc989409b6af05f18424489f7
Luis Alejandro <lpascuazac@unal.edu.co>
2021-07-26 23:20:29 -0500

    Four mics implemented using digital input detection

--- Main_mic_detection.ino
+++ Main_mic_detection.ino
@@ -7,13 +7,32 @@
 BluetoothSerial SerialBT;
 
 int led = 2;           //Conected to pin 2. Output. Built in led
-int sound_digital = 0; //Conected to pin 0. Input
-int sound_analog = 4;  //Conected to pin 4. Input
+
+//Mic 1
+int sound_digital_1 = 0; //Conected to pin 0. Input
+int sound_analog_1 = 4;  //Conected to pin 4. Input
+
+//Mic 2
+int sound_digital_2 = 36; //Conected to pin 36. Input
+int sound_analog_2 = 39;  //Conected to pin 39. Input
+
+//Mic 3
+int sound_digital_3 = 34; //Conected to pin 34. Input
+int sound_analog_3 = 35;  //Conected to pin 35. Input
+
+//Mic 4
+int sound_digital_4 = 32; //Conected to pin 32. Input
+int sound_analog_4 = 33;  //Conected to pin 33. Input
+
 
 void setup(){
   Serial.begin(115200);
   pinMode(led, OUTPUT);
-  pinMode(sound_digital, INPUT);
+  pinMode(sound_digital_1, INPUT);
+  pinMode(sound_digital_2, INPUT);
+  pinMode(sound_digital_3, INPUT);
+  pinMode(sound_digital_4, INPUT);
+  
   delay(3000);
   Serial.println("Starting BT...");
   SerialBT.begin("MyESP32"); //Bluetooth device name
@@ -22,17 +41,26 @@ void setup(){
 
 void loop(){
   
-  int val_digital = digitalRead(sound_digital);
-  int val_analog = analogRead(sound_analog);
+  int val_digital_1 = digitalRead(sound_digital_1);
+  int val_analog_1 = analogRead(sound_analog_1);
 
-  Serial.print(val_analog);
-  Serial.print("\t");
-  Serial.println(val_digital);
+  int val_digital_2 = digitalRead(sound_digital_2);
+  int val_analog_2 = analogRead(sound_analog_2);
 
-  if (val_digital == HIGH)
+  int val_digital_3 = digitalRead(sound_digital_3);
+  int val_analog_3 = analogRead(sound_analog_3);
+
+  int val_digital_4 = digitalRead(sound_digital_4);
+  int val_analog_4 = analogRead(sound_analog_4);
+
+  //Serial.print(val_analog);
+  //Serial.print("\t");
+  //Serial.println(val_digital);
+
+  if ( (val_digital_1 == HIGH)||(val_digital_2 == HIGH)||(val_digital_3 == HIGH)||(val_digital_4 == HIGH) )
   { 
     if (Serial.available()) {
-      SerialBT.println("Sound Detected");;
+      SerialBT.println("Sound Detected"); //Detection message send via bluetooth
     }
     
     digitalWrite (led, HIGH);
